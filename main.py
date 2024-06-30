c1180d7fb3f18c2d349b5c156a4959b84b8e78a1
gerb07 <markgerber13@icloud.com>
2024-06-30 22:30:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+my_dict = {'max': 89093454357, 'andrey': 89567839371, 'matvey': 87132378090}
+print(my_dict)
+print(my_dict['andrey'])
+my_dict.update({'danil': 89593983901,
+                 'oleg':59402855924})
+print(my_dict.get('dasha'))
+del my_dict ['matvey']
+print(my_dict)
+
+my_set = [1, 2, 2, 2, 3, 'lesha', {2, 3, 4, 3, 4}]
+list = {1, 1, 1, 1, 1, 2, 3, 4}
+print(set(list))
+print(list.add(7))
+print(list)
+print(list.remove(3))
+print(list)
+
+
